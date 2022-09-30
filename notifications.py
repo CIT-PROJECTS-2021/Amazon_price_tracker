b4a2415c3d0f538b887ad1f029e01f1bd33fe7ca
diphan-source <ahabwediphan@yahoo.com>
2022-09-30 13:29:19 +0300

    fix secret-key expose

--- notifications.py
+++ notifications.py
@@ -1,23 +1,34 @@
+import os
 import smtplib
 from email.message import EmailMessage
 import time 
+from dotenv import load_dotenv
+load_dotenv()
 
 
-EMAIL_ADDRESS = 'carlperez491@gmail.com' # email of the sender
-PASSWORD = 'nfttwqekspouvnnp' # password of the sender
-def send_mail(user_email, current_price):
+# Configure your Email
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+email_smtp_server = "smtp.example.com"
+email_smtp_port = 587
+# Configure End
+
+def send_mail(address, current_price):
+    receiver = address 
     msg = EmailMessage()
-    msg['Subject'] = 'Price Alert'
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = user_email
+    msg['Subject'] = 'Price Dropped!'
+    msg['From'] = "Amazon Price Tracker"
+    msg['To'] = receiver
     msg.set_content(f"The price of the commodity is USD. {current_price}")
     
     while True:
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login(EMAIL_ADDRESS, PASSWORD)
+        with smtplib.SMTP_SSL(email_smtp_server, email_smtp_server) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
             smtp.send_message(msg)
-        time.sleep(5*60*60) # notification will be sent every after five hours
-print(send_mail('charles.mcw@yahoo.com', 2350.0))
+        time.sleep(2*60*60) # notifications every after 2 hours
+        return send_mail
+
+    
 
 
